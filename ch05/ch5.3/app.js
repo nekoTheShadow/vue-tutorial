2f1a41b35e2133e358649966dba452989581a337
nekoTheShadow <h.nakamura0903@gmail.com>
2020-03-22 19:07:19 +0900

    5.3.3 フック関数の引数

--- ch05/ch5.3/app.js
+++ ch05/ch5.3/app.js
@@ -1,11 +1,21 @@
 Vue.directive('fallback-image', {
   bind: function(el) {
-    el.addEventListener('error', function(){
+    el.addEventListener('error', function(el, binding){
+      console.log('bind', binding)
       el.src = "https://dummyimage.com/400x400/000/fff.png&text=no+image";
     })
+  },
+
+  update: function(el, binding) {
+    console.log('update', binding);
   }
 })
 
-new Vue({
-  el: '#app'
+var vm = new Vue({
+  el: '#app',
+  data: function() {
+    return {
+      altText: 'logo'
+    }
+  }
 })
