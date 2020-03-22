694bcc9b8b41cc0899ece1eb787b5da8875ce7b4
nekoTheShadow <h.nakamura0903@gmail.com>
2020-03-22 19:19:00 +0900

    5.3.4 image-fallbackディレクティブの機能追加

--- ch05/ch5.3/app.js
+++ ch05/ch5.3/app.js
@@ -1,13 +1,20 @@
 Vue.directive('fallback-image', {
-  bind: function(el) {
-    el.addEventListener('error', function(el, binding){
-      console.log('bind', binding)
-      el.src = "https://dummyimage.com/400x400/000/fff.png&text=no+image";
+  bind: function(el, binding) {
+    console.log('bind', binding);
+    var once = binding.modifiers.once;
+    el.addEventListener('error', function onError(){
+      el.src = binding.value;
+      if (once) {
+        el.removeEventListener('error', onError);
+      }
     })
   },
 
   update: function(el, binding) {
     console.log('update', binding);
+    if (binding.oldVelue !== binding.value && binding.oldVelue === el.src) {
+      el.src = binding.value;
+    }
   }
 })
 
@@ -15,7 +22,8 @@ var vm = new Vue({
   el: '#app',
   data: function() {
     return {
-      altText: 'logo'
+      altText: 'logo',
+      noImageURL: "https://dummyimage.com/400x400/000/fff.png&text=no+image"
     }
   }
 })
