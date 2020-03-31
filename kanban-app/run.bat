4640cab60809e61e09c3f9f2c7d312b3390a3850
hajime nakamura <h.nakamura0903@gmail.com>
2020-04-01 01:25:03 +0900

    8.8.4 バックエンドAPIサーバーの環境構築

--- kanban-app/run.bat
+++ kanban-app/run.bat
@@ -3,7 +3,7 @@
 REM http://192.168.99.100:8080/
 
 set SUBCMD=
-set /p SUBCMD="npm run ??? ---> "
+set /p SUBCMD="Sub Command ? ---> "
 
 docker rmi kanban-app-image 
 docker build -f docker_%SUBCMD% -t kanban-app-image .
