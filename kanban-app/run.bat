dbd0a8d6c9415e0f83c310b390bf426427fd5804
hajime nakamura <h.nakamura0903@gmail.com>
2020-04-03 01:08:40 +0900

    IMPROVE

--- kanban-app/run.bat
+++ kanban-app/run.bat
@@ -5,10 +5,19 @@ REM http://192.168.99.100:8080/
 set SUBCMD=
 set /p SUBCMD="Sub Command ? ---> "
 
-docker rmi kanban-app-%SUBCMD%-image 
-docker build -f docker_%SUBCMD% -t kanban-app-%SUBCMD%-image .
+set t=%time: =0%
+
+set yyyy=%date:~0,4%
+set mm=%date:~5,2%
+set dd=%date:~8,2%
+set hh=%t:~0,2%
+set nn=%t:~3,2%
+set ss=%t:~6,2%
+
+set imagename=kanban-app-%SUBCMD%-%yyyy%%mm%%dd%%hh%%nn%%ss%
+
+docker build -f docker_%SUBCMD% -t %imagename% .
 docker run -it ^
            --rm ^
            -p 8080:8080 ^
-           --name kanban-app-%SUBCMD%-container ^
-           kanban-app-%SUBCMD%-image
+           %imagename%
